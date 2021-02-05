7fd7ad481fe3d0a7179948928864da86accf0532
k4k4rot0 <k4k4rot0@protonmail.com>
2021-02-05 14:11:37 -0300

    Adicionado Script

--- .config/polybar/scripts/launch.sh
+++ .config/polybar/scripts/launch.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env sh
-# Credit: Franklin Souza
-# Telegram: @FranklinTech
 
-# Script para subir a polybar
-
-# Se quiser só a bar de baixo, comenta a (polybar -q top &) se quiser só a bar de cima comenta a (polybar -q bottom &)
-
-# E se quiser as duas, deixa ambas descomentas
-
-# Terminar instâncias de barra já em execução
+# Encerrar instâncias de bar já em execução.
 killall -q polybar
 
-# Aguarde até que os processos sejam encerrados
+# Espere até que os processos sejam encerrados.
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Barra de lançamento top e barra bottom.
 
 polybar -q top &
 polybar -q bottom &
+
+echo "Barras lançadas ..."
