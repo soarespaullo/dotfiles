0cb6f272fd4020ae77c617d8932724e28bd2800c
k4k4rot0 <k4k4rot0@protonmail.com>
2021-03-31 22:18:29 -0300

    Up

---
+++ .config/polybar/scripts/powermenu.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+
+dir="~/.config/polybar/scripts/rofi"
+uptime=$( uptime --pretty | sed 's/up //' | sed 's/\ years\?,/y/' | sed 's/\ weeks\?,/w/' | sed 's/\ days\?,/d/' | sed 's/\ hours\?,\?/h/' | sed 's/\ minutes\?/m/')
+
+rofi_command="rofi -theme $dir/powermenu.rasi"
+
+# Opções
+shutdown=" Desligar"
+reboot=" Reiniciar"
+lock=" Bloquear tela"
+suspend=" Hibernar"
+logout=" Fazer Logoff"
+
+
+# Confirmação
+confirm_exit() {
+	rofi -dmenu\
+	-i\
+	-no-fixed-num-lines\
+	-p " Tem certeza? : "\
+	-theme $dir/confirmacao.rasi
+}
+
+
+# Mensagem
+msg() {
+	rofi -theme "$dir/mensagem.rasi" -e " Opções disponíveis são: - sim / s / nao / n"
+}
+
+
+# Variável passada para rofi
+options="$lock\n$suspend\n$logout\n$reboot\n$shutdown"
+
+
+chosen="$(echo -e "$options" | $rofi_command -p "Tempo de Atividade: $uptime" -dmenu -selected-row 0)"
+case $chosen in
+    $shutdown)
+		ans=$(confirm_exit &)
+		if [[ $ans == "sim" || $ans == "SIM" || $ans == "s" || $ans == "S" ]]; then
+			systemctl poweroff
+		elif [[ $ans == "nao" || $ans == "NAO" || $ans == "n" || $ans == "N" ]]; then
+			exit 0
+        else
+			msg
+        fi
+        ;;
+    $reboot)
+		ans=$(confirm_exit &)
+		if [[ $ans == "sim" || $ans == "SIM" || $ans == "s" || $ans == "S" ]]; then
+			systemctl reboot
+		elif [[ $ans == "nao" || $ans == "NAO" || $ans == "n" || $ans == "N" ]]; then
+			exit 0
+        else
+			msg
+        fi
+        ;;
+    $lock)
+		if [[ -f /usr/bin/lockscreen ]]; then
+			i3lock-color
+		elif [[ -f /usr/bin/betterlockscreen ]]; then
+			betterlockscreen -l
+		fi
+        ;;
+    $suspend)
+		ans=$(confirm_exit &)
+		if [[ $ans == "sim" || $ans == "SIM" || $ans == "s" || $ans == "S" ]]; then
+			mpc -q pause
+			amixer set Master mute
+			systemctl suspend
+		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+			exit 0
+        else
+			msg
+        fi
+        ;;
+    $logout)
+		ans=$(confirm_exit &)
+		if [[ $ans == "sim" || $ans == "SIM" || $ans == "s" || $ans == "S" ]]; then
+			if 
+			[[ "$DESKTOP_SESSION" == "i3" ]]; then
+				i3-msg exit
+			fi
+		elif [[ $ans == "nao" || $ans == "NAO" || $ans == "n" || $ans == "N" ]]; then
+			exit 0
+        else
+			msg
+        fi
+        ;;
+esac
